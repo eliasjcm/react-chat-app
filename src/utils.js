b68581abce8d9d6e299bc51e380c088de85e8818
Elias C <eliasc20022002@gmail.com>
2022-07-23 02:04:08 -0600

    HOSTNAME FIX

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-export const HOSTNAME = "http://192.168.100.47:5000"
+// export const HOSTNAME = "http://192.168.100.47:5000"
 export const CALL_DEVELOPMENT = false;
 
-// export const HOSTNAME = "https://node-chat-app-tst.herokuapp.com"
+export const HOSTNAME = "https://node-chat-app-tst.herokuapp.com"
