9b270024ae5e367370312e53114dd74076f3fcbc
Elias <eliasc20022002@gmail.com>
2023-04-04 12:05:15 -0600

    New Buildpack Test

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 // export const HOSTNAME = "http://localhost:5000"
 // export const HOSTNAME = "http://192.168.100.158:5000"
-export const HOSTNAME = "https://node-chat-app-tst.herokuapp.com"
+export const HOSTNAME = "https://node-chat-app-tst.herokuapp.com";
 // export const CALL_DEVELOPMENT = true;
 export const CALL_DEVELOPMENT = false;
